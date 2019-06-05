db49e925bf715c529f2ae6197493cd0b856fe8c4
EC2 Default User <yamini.patel41186@yahoo.com>
2019-06-05 08:25:55 +0000

    Implrmrnt advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,4 +67,8 @@ test "name should be present" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+  
+   test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
