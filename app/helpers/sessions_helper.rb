63b133f1a08d7683e534b1d311a0d01f6815490e
EC2 Default User <yamini.patel41186@yahoo.com>
2019-06-10 11:52:32 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,6 +8,9 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
+  def current_user?(user)
+    user == current_user 
+  end
   
    def current_user
     if (user_id = session[:user_id])
@@ -34,5 +37,11 @@ module SessionsHelper
       session.delete(:user_id)
       @current_user = nil
   end
-  
-end
+   def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+   def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  end
