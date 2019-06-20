b66b26c29987a1423f7a318d23161e621f8eb76a
EC2 Default User <yamini.patel41186@yahoo.com>
2019-06-20 03:56:30 +0000

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<rocky-dusk-85862>.herokuapp.com'
+  host = 'rocky-dusk-85862.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
