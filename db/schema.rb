b329954b275763a9ba49251915211a54cbf2bb80
EC2 Default User <yamini.patel41186@yahoo.com>
2019-06-21 14:19:31 +0000

    Add user following

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190619151537) do
-ActiveRecord::Schema.define(version: 20190621060324) do
+ActiveRecord::Schema.define(version: 20190621081544) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -19,9 +18,18 @@ ActiveRecord::Schema.define(version: 20190621060324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "picture"
-
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,6 +47,5 @@ ActiveRecord::Schema.define(version: 20190621060324) do
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-end
-end
 
+end
